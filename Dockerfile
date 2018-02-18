febabc8f3b0a7c1a55684ca61a3eca63503ee65a
letssudormrf <letssudormrf@gmail.com>
2018-02-18 18:04:33 +0800

    update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM alpine:3.7
 
 # Add local files to image
 COPY files /
+RUN chmod a+rwx /usr/local/bin/entrypoint.sh
     
 RUN set -ex;\
     apk update;\
@@ -10,4 +11,4 @@ RUN set -ex;\
     rm -rf /var/cache/apk/*
 
 ENTRYPOINT ["/sbin/tini", "--", "entrypoint.sh"]
-CMD ["sniproxy", "-f"]
+CMD ["sniproxy", "-c /tmp/sniproxy.conf", "-f"]
--- files/usr/local/bin/entrypoint.sh
+++ files/usr/local/bin/entrypoint.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
-cp /etc/sniproxy.tmpl /etc/sniproxy.conf
+if [ -n "${USER}" ]; then
+echo "user ${USER}" >> /tmp/sniproxy.conf
+fi
+
+if [ -n "${GROUP}" ]; then
+echo "group ${GROUP}" >> /tmp/sniproxy.conf
+fi
+
+if [ -n "${DNS}" ]; then
+awk -v rules="${DNS}" 'BEGIN {
+    split (rules, sections, " ");
+    print "resolver {"
+    for (section in sections) {
+        split (sections[section], rule, ";")
+        print "   " rule[1] " " rule[2]
+    }
+    print "}\n"
+}' >> /tmp/sniproxy.conf
+fi
 
 awk -v listeners="${LISTENERS}" 'BEGIN {
     split (listeners, sections, " ");
@@ -8,7 +26,7 @@ awk -v listeners="${LISTENERS}" 'BEGIN {
         split (sections[section], listener, ";")
         print "listener " listener[2] " {\n   proto " listener[1] "\n   table " listener[1] "\n}\n"
     }
-}' >> /etc/sniproxy.conf
+}' >> /tmp/sniproxy.conf
 
 awk -v rules="${RULES_HTTP}" 'BEGIN {
     split (rules, sections, " ");
@@ -18,7 +36,7 @@ awk -v rules="${RULES_HTTP}" 'BEGIN {
         print "   " rule[1] " " rule[2]
     }
     print "}\n"
-}' >> /etc/sniproxy.conf
+}' >> /tmp/sniproxy.conf
 
 awk -v rules="${RULES_TLS}" 'BEGIN {
     split (rules, sections, " ");
@@ -28,10 +46,10 @@ awk -v rules="${RULES_TLS}" 'BEGIN {
         print "   " rule[1] " " rule[2]
     }
     print "}\n"
-}' >> /etc/sniproxy.conf
+}' >> /tmp/sniproxy.conf
 
 echo "*** Show sniproxy configuration ***"
-cat /etc/sniproxy.conf
+cat /tmp/sniproxy.conf
 
 echo "*** Startup $0 suceeded now starting service using eval to expand CMD variables ***"
 exec su-exec sniproxy $(eval echo "$@")
